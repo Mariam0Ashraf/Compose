f1550cbe0fee64f17c1d7ac3e7efe8b1037485c3
mariam0ashraf <mariam00ashraf@gmail.com>
2024-12-05 15:35:36 -0500

    add 2 buttons

--- app/src/main/java/com/example/acomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/acomposeapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.acomposeapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,30 +9,32 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.acomposeapp.ui.theme.AComposeAppTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AComposeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Mariam",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    Counter()
                 }
             }
         }
@@ -39,33 +42,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    var myName by remember{ mutableStateOf("") }
+fun Counter() {
+    var counter by rememberSaveable { mutableIntStateOf(0) }
+
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
-
-
-    Text(
-        text = "Hello please enter your name!",
-        modifier = modifier
-        //fontSize =28.sp
-    )
-    TextField(
-        value= myName,
-        onValueChange = {
-            myName=it
+    ) {
+        Button(onClick = { counter++ }) {
+            Text("Increase")
         }
-    )
-}
+        Text(
+            text = counter.toString(),
+            fontSize = 64.sp
+        )
+        Button(onClick = { counter-- }) {
+            Text("Decrease")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AComposeAppTheme {
-        Greeting("Android")
+        Counter()
     }
 }
